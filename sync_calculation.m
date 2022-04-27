3c23beb5407d433bd25d7b0ba950aa0cebcb1b79
Artur Czeszumski <aczeszumski@uos.de>
2022-04-27 11:49:26 +0900

    add paths and load EEG and triggers

--- sync_calculation.m
+++ sync_calculation.m
@@ -1,11 +1,19 @@
-
+% add required libraries to the path
+addpath(genpath('D:\Dropbox\MATLAB_Tools'))
+%change the path to the folder with scripts and add the folder to the path
+cd D:\Dropbox\Projects\BMK_Japan
+addpath(genpath('D:\Dropbox\Projects\BMK_Japan'))
 
 % path to file and folder
 path_to_file = 'C:\Users\aczeszumski\Documents\CurrentStudy\sub-P001\ses-S001\eeg\sub-P001_ses-S001_task-Default_run-001_eeg.xdf';
 path_new_folder = 'C:\Users\aczeszumski\Documents\CurrentStudy\sub-P001\ses-S001\eeg\\MOBI';
-% load streams and combine them in one struct
+
+% load EEG streams and combine them in one struct
 mobilab.allStreams = dataSourceXDF(path_to_file, path_new_folder);
 all_mobilab_streams = [mobilab.allStreams().item];
+
+% load triggers timing
+d = readtable('results.csv')
 
 % stream A - quick30
 
