3c52d7677708a03bab49bf9d3101ea4f82b879a7
Artur Czeszumski <aczeszumski@uos.de>
2022-04-28 12:23:28 +0900

    remove code related to manual triggers CGX30

--- sync_calculation.m
+++ sync_calculation.m
@@ -20,51 +20,15 @@ d = readtable('results.csv');
 triggers_CGX30 = zeros(length(all_mobilab_streams{1,3}.data(:,30)),1); % vector with zeros
 all_mobilab_streams{1,3}.data(:,31) = all_mobilab_streams{1,3}.auxChannel.data(:,4); %temporary store triggers signal as channel 31
 
-% create a list of events to remove events really close to each other
-% (wrong collection of triggers by CGX, so each trigger was repeated over
-% consecutive timestamps (around 10))
 
-ts_list = find(all_mobilab_streams{1,3}.data(:,31)~=0); %find all events
-ts_list_unique = ts_list; % empty list for unique events
-% for loop to remove all triggers that are repeated over consecutive
-% timestamps
-for idx = 2:length(ts_list)
-    if (ts_list(idx) - ts_list(idx-1)) < 5
-        ts_list_unique(idx)=0;
-    end
-end
-
-ts_list = unique(ts_list_unique); % select unique time stamps for each trigger
-ts_list = ts_list(2:end); % remove trigger #0
-triggers_CGX30([ts_list]) = 1; % add triggers as '1' to the empty vector
-all_mobilab_streams{1,3}.data(:,31) = triggers_CGX30; % add triggers to the channel 31
 all_mobilab_streams{1,3}.label(31) = {'trigger'}; % add label name for trigger channel
 all_mobilab_streams{1,3}.channelSpace = NaN(31,3); % expand channelSpace - it's all empty now but size has to be consistent with data struct
 
 % sync 2 eeg streams and markers
 exported_EEG = mobilab.allStreams().export2eeglab([1,3],[2]);
 
-% change trigger channel from CGX30quick into markers
-% comment: mobilab change triggers in CGX30 to different numbers so it has
-% to be corrected. First out of the 1-3 numbers is chosen for precision 
-exported_EEG.data(61,find(exported_EEG.data(61,:) ~= 0)) = 1;
 
-triggers_list = find(exported_EEG.data(61,:)~=0); %find all events
-triggers_list_unique = triggers_list; % empty list for unique events
-% for loop to remove all triggers that are repeated over consecutive
-% timestamps
-for idx = 2:length(triggers_list)
-    if (triggers_list(idx) - triggers_list(idx-1)) < 5
-        triggers_list_unique(idx)=0;
-    end
-end
-
-triggers_list = unique(triggers_list_unique); % select unique time stamps for each trigger
-triggers_list = triggers_list(2:end); % remove trigger #0
-exported_EEG.data(61,:) =0; %make trigger channel empty
-exported_EEG.data(61,triggers_list) = 1; % add triggers 
-% change triggers from channel to events field in tmp stuct
-tmp_EEG = pop_chanevent(exported_EEG, 61,'edge','leading','edgelen',0,'duration','on','nbtype',1); 
+tmp_EEG = pop_chanevent(exported_EEG, 61,'edge','leading','edgelen',1.1,'nbtype',1); 
 
 % remove fields that overlap between two events streams
 tmp_EEG.event = rmfield(tmp_EEG.event, 'urevent');
@@ -111,7 +75,7 @@ for event =1:length(CGXX32_markers.event.label)
     end
 end
 
-% triggers recevied == 551
+% triggers received == 551
 
 % triggers after LSL sync
 epoch_CGX32 = pop_epoch(exported_EEG,{'CGX32'},[0 ,2]);
@@ -119,4 +83,23 @@ size(epoch_CGX32.data,3)
 
 % triggers received == 551
 
-%%
+%% Step 2 - look at triggers latency in CGX30 - data collected via LSL + cloud
+
+% compare amount of events before synchronizing LSL streams
+d = d(2:end);
+d{:,1}=d{:,1}+1;
+size(d,1)
+% triggers sent == 595
+
+%triggers_before LSL sync
+size(ts_list,1)
+% triggers received == 595
+
+%triggers_after LSL sync
+epoch_CGX30 = pop_epoch(exported_EEG,{'CGX30'},[0 ,2]);
+size(epoch_CGX30.data,3)
+% triggers received == 545
+
+
+
+
