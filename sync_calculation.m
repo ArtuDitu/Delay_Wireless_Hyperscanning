34bf745d9f08665e7bd1cfb5d9fcc70d776fc5a2
Artur Czeszumski <aczeszumski@uos.de>
2022-04-28 12:40:45 +0900

    remove empty row from csv data file

--- sync_calculation.m
+++ sync_calculation.m
@@ -19,27 +19,27 @@ d = readtable('results.csv');
 %add triggers as channel 31 in CGX30quick system
 triggers_CGX30 = zeros(length(all_mobilab_streams{1,3}.data(:,30)),1); % vector with zeros
 all_mobilab_streams{1,3}.data(:,31) = all_mobilab_streams{1,3}.auxChannel.data(:,4); %temporary store triggers signal as channel 31
-
-
 all_mobilab_streams{1,3}.label(31) = {'trigger'}; % add label name for trigger channel
 all_mobilab_streams{1,3}.channelSpace = NaN(31,3); % expand channelSpace - it's all empty now but size has to be consistent with data struct
 
 % sync 2 eeg streams and markers
 exported_EEG = mobilab.allStreams().export2eeglab([1,3],[2]);
 
-
+% make events from channel 31 CGX30
 tmp_EEG = pop_chanevent(exported_EEG, 61,'edge','leading','edgelen',1.1,'nbtype',1); 
 
 % remove fields that overlap between two events streams
 tmp_EEG.event = rmfield(tmp_EEG.event, 'urevent');
 exported_EEG.event = rmfield(exported_EEG.event, 'urevent');
 exported_EEG.event = rmfield(exported_EEG.event, 'hedTag');
+exported_EEG.event = rmfield(exported_EEG.event, 'duration');
 % combine triggers
 combine_triggers_2systems = [exported_EEG.event tmp_EEG.event]; % combined array
 [~,index] = sortrows([combine_triggers_2systems.latency].'); % sort by latency
 combine_triggers_2systems = combine_triggers_2systems(index); clear index %sort by latency
 exported_EEG.event = combine_triggers_2systems; %add to event struct
 exported_EEG.urevent = []; % remove urvent field
+
 exported_EEG = eeg_checkset(exported_EEG); % check set
 
 % remove obsolete triggers and rename existing once
@@ -56,12 +56,12 @@ for event =1:length(exported_EEG.event)
 end
 exported_EEG.event(events_delete_list) = []; % remove 
 
-eegplot(exported_EEG.data,'srate',exported_EEG.srate,'eloc_file',exported_EEG.chanlocs,'events',exported_EEG.event);
+%eegplot(exported_EEG.data,'srate',exported_EEG.srate,'eloc_file',exported_EEG.chanlocs,'events',exported_EEG.event);
 
 %% Step 1 - look at triggers latency in CGX32 - data collected via LSL and wireless local network
 
 % compare amount of events before synchronizing LSL streams
-d = d(2:end);
+d = d(2:end,:);
 d{:,1}=d{:,1}+1;
 size(d,1)
 % triggers sent == 595
@@ -85,20 +85,17 @@ size(epoch_CGX32.data,3)
 
 %% Step 2 - look at triggers latency in CGX30 - data collected via LSL + cloud
 
-% compare amount of events before synchronizing LSL streams
-d = d(2:end);
-d{:,1}=d{:,1}+1;
-size(d,1)
 % triggers sent == 595
 
 %triggers_before LSL sync
-size(ts_list,1)
+
+
 % triggers received == 595
 
 %triggers_after LSL sync
 epoch_CGX30 = pop_epoch(exported_EEG,{'CGX30'},[0 ,2]);
 size(epoch_CGX30.data,3)
-% triggers received == 545
+% triggers received == 587
 
 
 
